6a6224a38525193b104c8232f6b000155f8fbbd4
ezzedin779 <abdou.babchia64@gmail.com>
2021-02-07 22:33:59 +0100

    quick lomuto sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,73 @@
+#include "sort.h"
+/**
+*quick_sort - quick sort with lomuto partition scheme
+*@array: the array to sort
+*@size: size of the array
+*Return: sorted array
+*/
+void quick_sort(int *array, size_t size)
+{
+	if (size < 2)
+		return;
+	quicksort(array, 0, size - 1, size);
+}
+
+/**
+*partition_lomuto - the partition of lomuto
+*@A: array to part
+*@lo: hiest value
+*@hi: loest value
+*Return: position
+*/
+int partition_lomuto(int *A, int lo, int hi, size_t size)
+{
+	int pivot = A[hi];
+	int j = lo - 1;
+	int i;
+
+	for (i = lo; i < hi; i++)
+		if (A[i] < pivot)
+		{
+			j = j + 1;
+			swap(&A[i], &A[j]);
+			if (j != i)
+				print_array(A, size);
+		}
+	swap(&A[hi], &A[j + 1]);
+	if (hi != (j + 1))
+		print_array(A, size);
+	return (j + 1);
+}
+
+/**
+*swap - swap two elements of an array
+*@a: the first element
+*@b: second element
+*Return: a is b and b is a
+*/
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
+
+/**
+*quicksort - quick sort with 3 elements to pass
+*@A: array
+*@lo: hiest
+*@hi: loest
+*Return: noth
+*/
+void quicksort(int *A, int lo, int hi, size_t size)
+{
+	int p;
+
+	if (lo < hi)
+	{
+		p = partition_lomuto(A, lo, hi, size);
+		quicksort(A, lo, p - 1, size);
+		quicksort(A, p + 1, hi, size);
+	}
+}
