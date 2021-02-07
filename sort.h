413c1c46f2d87e10fda3cdc17c44701683f3b291
ezzedin779 <abdou.babchia64@gmail.com>
2021-02-07 22:34:09 +0100

    HEADER 0.4

--- sort.h
+++ sort.h
@@ -24,5 +24,8 @@ void bubble_sort(int *array, size_t size);
 void swap(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int partition_lomuto(int *A, int lo, int hi, size_t size);
+void quicksort(int *A, int lo, int hi, size_t size);
 
 #endif
